c1231a2b4b90346e44961dc5aef2b45bae57528e
Supriya-KK <supriyakhadka060@gmail.com>
2025-06-02 20:16:14 +0530

    Initial projects files added

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# Resume-Analyzer-App
+# Resume-Analyzer-App (Streamlit App)
+This is a simple web app built using **Python** and **Streamlit** that analyzers your resume for key skills.
+
+## What it does
+    - Upload your resume (PDF format)
+    - It scans for 20+ important skills 
+    (e.g., Python, SQL, Gir, AWS, ML, etc.)
+    -shows:
+        -Skills found
+        -Skills missing
+        -Resume score (Based on how many skills matched)
+
+## Purpose
+Help students, freshers, and job seekers improve their resume by identifying missing keywords that companies look for!
+
+## Built with
+    -Python
+    -Streamlit
+    -PyPDF2
+    -Regex
---
+++ app.py
@@ -0,0 +1,39 @@
+import streamlit as st
+import PyPDF2
+import re
+
+st.set_page_config(page_title= "Resume Analyzer", layout="centered")
+
+st.title("Resume Analyzer")
+st.subheader("Upload your resume to check for essential tech skills!")
+
+keywords = ["Python", "C", "C++", "SQL", "Git", "GitHub", "AWS", "Cloud", "Excel", "Machine Learning", "Linux", "IoT"]
+
+uploaded_file= st.file_uploader("Upload your resume (PDF format only)", type="pdf")
+
+if uploaded_file is not None:
+    try:
+        pdf_reader = PyPDF2.PdfReader(uploaded_file)
+        full_text =""
+        for page in pdf_reader.pages:
+            text = page.extract_text()  # <-- FIXED: add () to extract_text
+            if text:
+                full_text += text
+        found = [kw for kw in keywords if re.search(rf"\b{kw}\b", full_text, re.IGNORECASE)]
+        missing = [kw for kw in keywords if kw not in found]
+        score =len(found)/ len(keywords)*100
+
+        st.success("Resume analyzed successfully!")
+        st.write(f"** Total Pages:**{len(pdf_reader.pages)}")
+        st.write(f"** Found Skills:**{', '.join(found)if found else 'None'}")
+        st.write(f"** Missing Skills:**{', '.join(missing)if missing else 'None'}")
+        st.write(f"** REsume Score:**{round(score, 2)}/100")
+
+        if score <50:
+            st.warning("Try adding more rekevant skills to your resume.")
+        else:
+            st.success("Great Job! Your resume covers many important skills.")
+        
+        
+    except Exception as e:
+        st.error(f"Something went wrong while reading the file: {e}")

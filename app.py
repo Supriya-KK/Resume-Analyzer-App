89123378af3eb9c4ab2e23fe92eb9ec36863ccbb
Supriya-KK <supriyakhadka060@gmail.com>
2025-06-06 19:46:19 +0530

    Added some new features

--- app.py
+++ app.py
@@ -2,14 +2,54 @@ import streamlit as st
 import PyPDF2
 import re
 
-st.set_page_config(page_title= "Resume Analyzer", layout="centered")
+st.set_page_config(page_title= "Resume Analyzer", layout="centered", page_icon="📄" )
+dark_mode=st.toggle ("🌙 Enable Dark mode")
+if dark_mode:
+    st.markdown("""
+        <style>
+        .main {
+            background-color: #121212;
+            color: #eeeeee
+        }
+    </style>   
 
-st.title("Resume Analyzer")
-st.subheader("Upload your resume to check for essential tech skills!")
+""", unsafe_allow_html=True)
+
+
+#Custom CSS
+st.markdown("""
+    <style>
+        .main {
+            background-color: #f9f9f9;
+        }
+        .stFileUploader {
+            border: 2px dashed #4a90e2;
+            padding: 10px;
+            background-color: #ffffff;
+        }
+        .title-style {
+            font-size:40px;
+            color: #4a90e2;
+            font-weight: bold;
+            text-align: center;
+
+        }
+        .subheader-style{
+            font-size:20px;
+            color: #333;
+            text-align: center;
+        }
+     </style>   
+
+""", unsafe_allow_html=True)
+
+st.markdown('<div class="title-style">📄 Resume Analyzer</div>', unsafe_allow_html=True)
+st.markdown('<div class="subheader-style"> Upload your resume to check for essential tech skills! </div>', unsafe_allow_html=True)
+st.markdown("---")
 
 keywords = ["Python", "C", "C++", "SQL", "Git", "GitHub", "AWS", "Cloud", "Excel", "Machine Learning", "Linux", "IoT"]
 
-uploaded_file= st.file_uploader("Upload your resume (PDF format only)", type="pdf")
+uploaded_file= st.file_uploader("📎 Upload your resume (PDF format only)", type="pdf")
 
 if uploaded_file is not None:
     try:
@@ -19,15 +59,25 @@ if uploaded_file is not None:
             text = page.extract_text()  # <-- FIXED: add () to extract_text
             if text:
                 full_text += text
+        with st.expander("Preview Extracted Resume Text"):
+            st.text(full_text)
+        
         found = [kw for kw in keywords if re.search(rf"\b{kw}\b", full_text, re.IGNORECASE)]
         missing = [kw for kw in keywords if kw not in found]
         score =len(found)/ len(keywords)*100
 
+        col1, col2= st.columns(2)
+        col1.metric("📄 Total Pages", len(pdf_reader))
+        col2.metric("📈Resume Score", f"{round(score, 2)} / 100")
+        st.progress(int(score))
+
         st.success("Resume analyzed successfully!")
-        st.write(f"** Total Pages:**{len(pdf_reader.pages)}")
-        st.write(f"** Found Skills:**{', '.join(found)if found else 'None'}")
-        st.write(f"** Missing Skills:**{', '.join(missing)if missing else 'None'}")
-        st.write(f"** Resume Score:**{round(score, 2)}/100")
+    
+        st.markdown("### ✅ Found Skills: ")
+        st.write(", ".join(found)if found else "None")
+        st.markdown("### Missing Skills: ")
+        st.write(", ".join(missing)if missing else "None")
+        
 
         if score <50:
             st.warning("Try adding more relevant skills to your resume.")
@@ -37,3 +87,5 @@ if uploaded_file is not None:
         
     except Exception as e:
         st.error(f"Something went wrong while reading the file: {e}")
+
+
